97889a4e8e5d4813da06c86a6e0022b8447b208b
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-10-19 12:12:16 +0530

    commmit success

--- src/Calculator/Calculate.java
+++ src/Calculator/Calculate.java
@@ -7,7 +7,7 @@ public class Calculate {
   Addition a=new Addition();
   a.add(5,6);
   Subtraction s=new Subtraction();
-  s.sub(7, 2);
+  s.sub(10, 2);
   Multiplication m=new Multiplication();
   m.mul(3, 4);
 	}
